22c9f4c9bca88f9b80af3aec7f7d3ed0a3b98fa1
UrkeNzY <programerurke@gmail.com>
2023-01-28 17:34:24 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+// 1)
+ let courseName = "The Odin Project";
+ let coursePrice = 10;
+ let courseGoals = ["learn to code", "apply to an IT university", "get a job as a programmer"];
+
+// 2)
+ alert(courseName);
+ alert(coursePrice);
+ alert(courseGoals);
+
+ //3)
+//   let course = 
+//   { 
+//       name: "The Odin Project", 
+//       price: 10, 
+//       goals: ["learn to code", "apply to an IT university", "get a job as a programmer"] 
+//   };
+
+// alert(course.name);
+// alert(course.price);
+// alert(course.goals);
+
+// 4)
+// alert(course.goals[1]);
+
+// 5)
+// function getArrayElement(array, arrayIndex) {
+//     let arrayElement = array[arrayIndex];
+//     return arrayElement;
+// }
+
+// 6)
+// let firstElement = getArrayElement(course.goals, 0);
+// alert(firstElement);
